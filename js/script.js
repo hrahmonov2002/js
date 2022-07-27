da6b9eedafb6f3e4ad43c8fe6bfebd97738cbcab
hrahmonov2002 <107402912+hrahmonov2002@users.noreply.github.com>
2022-07-27 13:28:28 +0500

    Add js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var intro = alert("Xush kelibsz!");
+var first_name = prompt("Ismingiz nima?"). trim();
+var last_name = prompt("Familyangiz nima?"). trim();
+var age = prompt("Yoshingiz nechida?"). trim();
+var job = prompt("Kasbingizni kiriting!"). trim();
+var home = prompt("Yashash joyingizni kiriting?"). trim();
+var alert = alert("Rahmat");
+
+console.log(`Ismingiz: ${first_name} 
+
+Familyangiz: ${last_name} 
+
+Yoshingiz: ${age} 
+
+Kasbingiz: ${job} 
+
+Yashash manzilingiz: ${home}`);

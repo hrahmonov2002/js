ce237811d51d6ea0205fa9b3ef54ea90913363d1
hrahmonov2002 <107402912+hrahmonov2002@users.noreply.github.com>
2022-07-29 21:21:35 +0500

    Add js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 var intro = alert("Xush kelibsz!");
-var first_name = prompt("Ismingiz nima?"). trim();
-var last_name = prompt("Familyangiz nima?"). trim();
-var age = prompt("Yoshingiz nechida?"). trim();
-var job = prompt("Kasbingizni kiriting!"). trim();
-var home = prompt("Yashash joyingizni kiriting?"). trim();
-var alert = alert("Rahmat");
+var first_name = prompt ("Ismingizni kiriting", "Hayitbek");
+var age = prompt("Iltimos yoshingizni kiriting", 18);
+var minAge = 16;
 
-console.log(`Ismingiz: ${first_name} 
+console.log(`Ismingiz: ${first_name}
+Yoshingiz: ${age}`);
 
-Familyangiz: ${last_name} 
+if (age >= minAge) {
+    alert('Saytimizga xush kelibsz')
+  } else if (age <= minAge) {
+    alert('Uzr sayt faqat kottalar uchun')
+  };
+    
+ 
 
-Yoshingiz: ${age} 
+// var intro = alert("Xush kelibsz!");
+// var first_name = prompt("Ismingiz nima?"). trim();
+// var last_name = prompt("Familyangiz nima?"). trim();
+// var age = prompt("Yoshingiz nechida?"). trim();
+// var job = prompt("Kasbingizni kiriting!"). trim();
+// var home = prompt("Yashash joyingizni kiriting?"). trim();
+// var alert = alert("Rahmat");
 
-Kasbingiz: ${job} 
+// console.log(`Ismingiz: ${first_name} 
 
-Yashash manzilingiz: ${home}`);
+// Familyangiz: ${last_name} 
+
+// Yoshingiz: ${age} 
+
+// Kasbingiz: ${job} 
+
+// Yashash manzilingiz: ${home}`);
